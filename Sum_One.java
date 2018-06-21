7e61646a185cec459a20c1f0b901e00855ceb472
janicegonsalves <40386522+janicegonsalves@users.noreply.github.com>
2018-06-21 17:34:16 +0530

    Add files via upload

---
+++ Sum_One.java
@@ -0,0 +1,14 @@
+package complete;
+
+public class Sum_One {
+	public void total(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+
+	public static void main(String[] args) {
+		Sum_One t=new Sum_One();
+		t.total(10,20);
+	}
+
+}
